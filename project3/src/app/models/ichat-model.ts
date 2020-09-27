b2cf0a5e2e3f5a30ea22663c3abdbe445fe4e8a5
arif1407013 <arifurrahmankislu@gmail.com>
2020-09-27 20:33:19 +0600

    Add files via upload

---
+++ project3/src/app/models/ichat-model.ts
@@ -0,0 +1,5 @@
+export interface IChatModel {
+    Message: string;
+    Owner: string;
+    Time: Date;
+}
---
+++ project3/src/app/models/iuser-model.ts
@@ -0,0 +1,6 @@
+export interface IUserModel {
+    Username: string;
+    Password: string;
+    Email: string;
+    Key?: string;
+}
